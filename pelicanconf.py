f52f0e6ed88e3065dc2b68dcb9df89ba15739f08
capu <juanpcapurro@gmail.com>
2019-05-30 22:15:42 -0300

    copied the simple theme with some bare css

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'capu'
+SITENAME = "capu's blog"
+SITEURL = ''
+SITE_TITLE = "[capu@blog]$"
+
+PATH = 'content'
+
+TIMEZONE = 'America/Argentina/Buenos_Aires'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+SOCIAL = ( ('github', 'http://getpelican.com/'),
+         ('twitter', 'http://python.org/') )
+
+# Social widget
+LINKS = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'themes/capu'
+
