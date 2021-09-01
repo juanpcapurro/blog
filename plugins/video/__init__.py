21fd7058cb0b4d7243da02ab79a0d246f63f083f
capu <hi@capu.tech>
2021-09-01 17:38:50 -0300

    updates to video plugin

--- plugins/video/__init__.py
+++ plugins/video/__init__.py
@@ -7,14 +7,17 @@ from docutils.parsers.rst import Directive, directives
 
 class Video(Directive):
     has_content = False
-    html = '<video controls muted>  <source src="%(src)s" type="video/webm">  </video>'
     required_arguments = 1
     optional_arguments = 0
+    option_spec = {'defaultaudio': bool}
     final_argument_whitespace = False
 
     def run(self):
-        self.options['src'] = directives.uri(self.arguments[0])
-        return [nodes.raw('', self.html % self.options, format='html')]
+        src = directives.uri(self.arguments[0])
+        extension = self.arguments[0].split('.')[-1]
+        muted = '' if 'defaultaudio' in self.options else 'muted'
+        html = f'<video controls {muted}>  <source src="{src}" type="video/{extension}">  </video>'
+        return [nodes.raw('', html , format='html')]
 
 def register():
     directives.register_directive('video', Video)
